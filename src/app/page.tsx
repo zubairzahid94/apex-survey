c6bf30ed39c9f66d0dbd60fea0e609b5d0fe62d5
Muhammad Saad <125290250+msaad53407@users.noreply.github.com>
2024-04-23 21:55:50 +0500

    Add pages

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Home() {
-  return (
-    <div className="w-full min-h-screen flex items-center justify-center">
-      <h1 className="text-h1 text-apex-grey-bluish">{"No Message"}</h1>
-    </div>
-  );
-}
