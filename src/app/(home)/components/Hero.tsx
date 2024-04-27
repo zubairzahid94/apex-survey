ea9163cc7480519c04bb3a673675fbdabb300131
Muhammad Saad <125290250+msaad53407@users.noreply.github.com>
2024-04-27 20:09:25 +0500

    Add Responsive to Hero Section

--- src/app/(home)/components/Hero.tsx
+++ src/app/(home)/components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative w-full">
+    <section className="relative w-full ">
       <div className="relative bg-white">
         <div className="absolute inset-0">
           <Image
@@ -20,9 +20,9 @@ export default function Hero() {
           />
           <div className="absolute inset-0 bg-black opacity-50" />
         </div>
-        <div className="relative w-3/4 px-4 sm:px-6 lg:px-8">
+        <div className="relative w-full md:w-3/4 px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center py-24 md:flex-row flex-col-reverse">
-            <div className="flex flex-col space-y-6 md:w-1/2">
+            <div className="flex flex-col space-y-6 w-full md:w-1/2">
               <h3 className="text-h3 text-white">
                 Empowering Your Property Journey with Insightful Surveys
               </h3>
@@ -43,10 +43,63 @@ export default function Hero() {
             </div>
           </div>
         </div>
+        <div className="bg-apex-grey-bluish relative md:absolute md:left-[40%] md:top-20 py-8 px-6 rounded-lg shadow-lg md:w-1/4">
+          <h4 className="text-btn text-white mb-4">We provide</h4>
+          <p className="text-para text-white mb-2">
+            Accurate Survey <span className="float-right">93%</span>
+          </p>
+          <Progress
+            className="w-full mb-4 bg-white text-apex-blue"
+            value={93}
+          />
+          <p className="text-para text-white mb-2">
+            Saving Your Time <span className="float-right">92%</span>
+          </p>
+          <Progress className="w-full  text-apex-blue" value={92} />
+        </div>
+        <div className="relative z-10 md:absolute md:right-8 md:-bottom-24">
+          <Image
+            src={"/pngwing 1.png"}
+            alt="Hero Image"
+            width={100}
+            height={100}
+            className="object-contain size-full md:size-[90%]"
+          />
+          <div className="bg-apex-grey-bluish flex gap-8 items-center h-40 px-8">
+            <div className="flex items-start gap-4 text-white">
+              <Image
+                src={"/icons/ribbon.svg"}
+                alt="Ribbon"
+                height={20}
+                width={20}
+              />
+              <div className="space-y-1">
+                <span className="text-h5">
+                  25<sup>+</sup>
+                </span>
+                <p className="text-para">Years of Experience</p>
+              </div>
+            </div>
+            <div className="flex items-start gap-4 text-white">
+              <Image
+                src={"/icons/person.svg"}
+                alt="Person"
+                height={20}
+                width={20}
+              />
+              <div className="space-y-1">
+                <span className="text-h5">
+                  2540<sup>+</sup>
+                </span>
+                <p className="text-para">Clients Satisfied</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div className="flex w-full h-max gap-12 items-center py-14 px-6 bg-apex-blue text-white">
-        <div className="flex gap-4 w-1/4">
+      <div className="flex flex-col md:flex-row w-full h-max gap-12 items-center py-14 px-6 bg-apex-blue text-white">
+        <div className="flex gap-4 w-full md:w-1/4">
           <Image
             src={"/icons/clipboard.svg"}
             alt="Clipboard"
@@ -61,7 +114,7 @@ export default function Hero() {
             </p>
           </div>
         </div>
-        <div className="flex gap-4 w-1/4">
+        <div className="flex gap-4 w-full md:w-1/4">
           <Image
             src={"/icons/ribbon.svg"}
             alt="Ribbon"
@@ -76,57 +129,6 @@ export default function Hero() {
             </p>
           </div>
         </div>
-      </div>
-
-      <div className="absolute right-8 bottom-24">
-        <Image
-          src={"/pngwing 1.png"}
-          alt="Hero Image"
-          width={100}
-          height={100}
-          className="object-contain size-[90%]"
-        />
-        <div className="bg-apex-grey-bluish flex gap-8 items-center h-40 px-8">
-          <div className="flex items-start gap-4 text-white">
-            <Image
-              src={"/icons/ribbon.svg"}
-              alt="Ribbon"
-              height={20}
-              width={20}
-            />
-            <div className="space-y-1">
-              <span className="text-h5">
-                25<sup>+</sup>
-              </span>
-              <p className="text-para">Years of Experience</p>
-            </div>
-          </div>
-          <div className="flex items-start gap-4 text-white">
-            <Image
-              src={"/icons/person.svg"}
-              alt="Person"
-              height={20}
-              width={20}
-            />
-            <div className="space-y-1">
-              <span className="text-h5">
-                2540<sup>+</sup>
-              </span>
-              <p className="text-para">Clients Satisfied</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="bg-apex-grey-bluish absolute left-[40%] top-20 py-8 px-6 rounded-lg shadow-lg md:w-1/4">
-        <h4 className="text-btn text-white mb-4">We provide</h4>
-        <p className="text-para text-white mb-2">
-          Accurate Survey <span className="float-right">93%</span>
-        </p>
-        <Progress className="w-full mb-4 bg-white text-apex-blue" value={93} />
-        <p className="text-para text-white mb-2">
-          Saving Your Time <span className="float-right">92%</span>
-        </p>
-        <Progress className="w-full  text-apex-blue" value={92} />
       </div>
     </section>
   );
