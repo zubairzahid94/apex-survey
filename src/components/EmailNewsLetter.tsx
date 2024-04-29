39d2cc3d6e311566076505c857e3d422aab5934e
Muhammad Saad <125290250+msaad53407@users.noreply.github.com>
2024-04-29 13:15:49 +0500

    Add Form handling in Email Newsletter

--- src/components/EmailNewsLetter.tsx
+++ src/components/EmailNewsLetter.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 import React from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 
 const EmailNewsLetter = () => {
+  const [email, setEmail] = React.useState("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(`Form submitted with email ${email}`);
+    setEmail("");
+  };
+
   return (
     <div className="px-6 py-12 flex flex-col md:flex-row items-center gap-8 bg-apex-blue w-[96%] mx-auto rounded-md mb-20">
       <div className="w-full md:w-1/2 flex flex-col gap-2">
@@ -15,12 +24,17 @@ const EmailNewsLetter = () => {
           tellus. At donec purus justo blandit at et nisl.{" "}
         </p>
       </div>
-      <form className="flex gap-2 w-full md:w-1/2 h-full items-center ">
+      <form
+        className="flex gap-2 w-full md:w-1/2 h-full items-center"
+        onSubmit={handleSubmit}
+      >
         <Input
           className="flex-1"
           name="email"
           placeholder="Your Email"
           type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <Button
           type="submit"
