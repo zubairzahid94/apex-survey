889b0b4b6e0c785f7ebd830a142dea48415749b2
Zubair Zahid <zubairzahid228@gmail.com>
2024-05-27 01:40:32 +0500

    checking

--- src/app/(ui)/(home)/page.tsx
+++ src/app/(ui)/(home)/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
       <Services />
       <section className="px-7 lg:px-11 flex flex-col gap-4 mb-4  max-w-screen-xl mx-auto">
         <div className="space-y-1">
-          <p className="text-para">Pricing</p>
+          <p className="text-para">Services</p>
           <h3 className="text-h3">Prices of Services We Offer</h3>
         </div>
         <div className="flex flex-wrap flex-row gap-2">
